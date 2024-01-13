69b326dfc34599bc30dbb942e2866e0b7303ee45
loddi <82147536+loddisjourney@users.noreply.github.com>
2024-01-13 20:27:38 +0100

    Place Line over time

--- talesofturtoise/Assets/Assets/Scripts/LoadingManager.cs
+++ talesofturtoise/Assets/Assets/Scripts/LoadingManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -10,19 +11,26 @@ public class LoadingManager : MonoBehaviour
 
     public GameObject loadingObj;
     int loadingInt = 12;
-    int positionOfZ = -6;
+    //ggf wieder -7
+    int positionOfZ = -8;
     GameObject currentPart;
     public GameObject loaderParent;
     int i = 0;
 
     public GameObject turtle;
     float speed = 0.3f;
+    public int placeTime = 3;
+    bool spawning;
+    float timer;
+
+    bool levelLoaded = false;
 
     void Start()
     {
         SceneManager.LoadScene("GeneratedLevel", LoadSceneMode.Additive);
         RenderSettings.skybox = skybox;
-        
+        timer = 3;
+        Debug.Log("timer " + timer);
     }
 
     private void Update()
@@ -34,16 +42,35 @@ public class LoadingManager : MonoBehaviour
 
         turtle.transform.Translate(Vector3.left * speed * Time.deltaTime);
 
-        while ( i < loadingInt)
-        {
-            currentPart = Instantiate(loadingObj, new Vector3(-27, 0, positionOfZ), Quaternion.Euler(0,-90,0));
-            currentPart.transform.parent = loaderParent.transform;
-            positionOfZ++;
-            i++;
-        }
-        
-        //Add end part and ask fpor progress => float progress =  current cound of cells / count of cells .. while progress != 1 do function -> is called from other script or is asked if some cells is collapsing
+        // while ( i < loadingInt)
+        // {
+        //     currentPart = Instantiate(loadingObj, new Vector3(-27, 0, positionOfZ), Quaternion.Euler(0,-90,0));
+        //    currentPart.transform.parent = loaderParent.transform;
+        //     positionOfZ++;
+        //     i++;
+        // }
+        Debug.Log("timer " + timer);
+        timer -= Time.deltaTime;
 
+           
+            if (timer <= 0.0f)
+            {
+                PlaceLine();
+            }
+
+
+            //Add end part and ask fpor progress => float progress =  current cound of cells / count of cells .. while progress != 1 do function -> is called from other script or is asked if some cells is collapsing
+            // over time until the scene is done
+
+        
+    }
+
+    public void PlaceLine()
+    {
+        currentPart = Instantiate(loadingObj, new Vector3(-27, 0, positionOfZ), Quaternion.Euler(0, -90, 0));
+        currentPart.transform.parent = loaderParent.transform;
+        positionOfZ++;
+        timer = 3;
     }
 
 }
